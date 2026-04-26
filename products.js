4925f53db9f1d59ec10e990e1b0b917a78c4cc78
Jianne <jianbach00001@gmail.com>
2026-04-26 16:12:58 +0700

    Create products.js
    source ở đây

---
+++ products.js
@@ -0,0 +1,20 @@
+const products = [
+  {
+    id: 1,
+    name: "Áo khoác Hàn Quốc",
+    image: "https://via.placeholder.com/300",
+    video: "https://www.tiktok.com/@abc/video/7271234567890123456",
+    desc: "Form đẹp, dễ phối đồ",
+    shopee: "https://shopee.vn/",
+    tiktok: "https://tiktok.com/"
+  },
+  {
+    id: 2,
+    name: "Áo thun basic",
+    image: "https://via.placeholder.com/300",
+    video: "https://www.tiktok.com/@abc/video/7271234567890123456",
+    desc: "Đơn giản nhưng chất",
+    shopee: "https://shopee.vn/",
+    tiktok: "https://tiktok.com/"
+  }
+];
